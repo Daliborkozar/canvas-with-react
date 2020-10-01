ecbace0e69e187bd88e3575a5079cf2ca79d80ab
dalibor kozar <kozardalibor@gmail.com>
2020-10-01 14:26:20 +0200

    meme generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const MemeGenerator = () => {
+
+  const [image, setImage] = useState(null)
+  const canvas = useRef(null)
+  const [topText, setTopText] = useState(' ')
+  const [bottomText, setBottomText] = useState(' ')
+
+  useEffect(() =>{
+    const catImage = new Image()
+    catImage.src ="https://picsum.photos/256"
+    catImage.onload =() => setImage(catImage)
+  },[])
+
+  useEffect(()=>{
+    if(image && canvas) {
+      const ctx = canvas.current.getContext('2d')
+      //Canvas Slika
+      ctx.fillStyle="black"
+      ctx.fillRect(0,0,400, 256+80)
+      ctx.drawImage(image, (400-256) /2,40)
+      //Canvas text
+      ctx.font = "30px Comic Sans MS"
+      ctx.fillStyle = "white"
+      ctx.textAlign = "center"
+
+      ctx.fillText(topText, 400/2, 30)
+      ctx.fillText(bottomText, 400/2, 256+40+30)
+    }
+  },[ image, canvas, topText, bottomText])
+
+  return(
+    <div>
+      <h1>Canvas Meme generator!</h1>
+      <div>
+        <canvas
+          ref={canvas}
+          width={400}
+          height={256+80}
+        />
+      </div>
+      <label>Enter Top Text: </label>
+      <input type='text'
+       value={topText}
+       onChange={e=> setTopText(e.target.value)}
+       />
+      
+      <br />
+      <label>Enter Bottom Text: </label>
+      <input type='text'
+       value={bottomText}
+       onChange={e=> setBottomText(e.target.value)}
+       />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default MemeGenerator
